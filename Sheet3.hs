a8bd3f917877dfce3b3cd5156f32fdc1996b139d
dominikkoller <mail@dominikkoller.net>
2017-11-07 01:02:56 +0000

    deleted unnec comments

--- Sheet3.hs
+++ Sheet3.hs
@@ -1,15 +1,6 @@
 import Data.Maybe
 
--- | Q 5.1 The predefined functions
--- | take :: Int -> [a] -> [a]
--- | drop :: Int -> [a] -> [a]
--- | divide a list into an initial segment and the rest, so that take n xs ++
--- | drop n xs = xs and take n xs is of length n or length xs, whichever is less.
-
--- | Write your own definitions for these functions and check that they give
--- | the same answer as the predefined functions for some representative
--- | arguments. Is take n xs strict in n? Is it strict in xs? Can it be strict
--- | in neither?
+-- | Q 5.1 
 
 take' :: Int -> [a] -> [a]
 take' _ [] = []
@@ -38,18 +29,7 @@ drop' n (x:xs) = drop' (n-1) xs
 -- | 5.2 Is map strict? Is map f strict?
 -- | TODO find definition of strict
 
--- | 5.3 Define a function evens :: [a] -> [a] which returns a list of the elements
--- | of its input that are in even numbered locations:
--- | *Main> evens [’a’..’z’]
--- | "acegikmoqsuwy"
--- | and a function odds of the same type which returns the remaining elements.
--- | (Hint: you might use the one function in defining the other. . . )
--- | Suppose you need both evens xs and odds xs for the same xs. Find an
--- | alternative definition for
--- | > alternates :: [a] -> ([a],[a])
--- | > alternates xs = (evens xs, odds xs)
--- | which calculates the result in a single pass along the list.
--- | Ideally, you should derive the definition showing that it is right.
+-- | 5.3
 
 evens :: [a] -> [a]
 evens (x:_:xs) = x:evens xs
@@ -88,27 +68,13 @@ alternates (x:y:xs) = (x:a, y:b)
 -- | 
 -- | Deriving from (evens xs, odds xs):
 -- | alternates xs       = (evens xs, odds xs)
--- | alternates (x:y:xs) = (evens [x:y:xs], odds [x:y:xs])
+-- | alternates (x:y:xs) = (evens (x:y:xs), odds (x:y:xs))
 -- |                     = (x:evens xs, y:odds xs)
 -- |                     = (x:a, y:b) where (a, b) = (evens xs, odds xs)
 -- |                     = (x:a, y:b) where (a, b) = alternates xs
 
 
--- | 6.1 Generalising an earlier exercise: 
--- | for finite types a, b and c there are as many functions of type a -> b -> c as there are are 
--- | of type (a, b) -> c (because (c^b)^a = c^ba ). 
--- | This correspondence, and a similar correspondence for infinte types, 
--- | is demonstrated by the (predefined) functions 
--- | 
--- | curry :: ((a,b) -> c) -> (a -> b -> c) 
--- | uncurry :: (a -> b -> c) -> ((a,b) -> c)
--- | 
--- | for which both curry.uncurry and uncurry.curry are identity functions (of the appropriate type). 
--- | These functions are uniquely determined by their types. 
--- | Write out what must be the definitions of these two functions, 
--- | and prove that they are mutually inverse. (If you type these definitions at an interpreter, 
--- | remember to change their names to avoid clashing with the Prelude functions.) 
--- | Does applying uncurry have any effect on strictness?
+-- | 6.1 
 
 -- | TODO find out the definition of strict
 -- | curry' :: ((a,b) -> c) -> a -> b -> c
@@ -130,12 +96,9 @@ alternates (x:y:xs) = (x:a, y:b)
 -- | 
 -- | Which shows that (curry.uncurry) and (uncurry.curry) are identity functions,
 -- | and that curry and uncurry are mutually inverse, since the definition inverse is:
--- | g is the inverse of f iff f.g is an identity function on the type of f.
+-- | g is the inverse of f iff f.g is an identity function on the type of the parameter of g.
 
--- | 6.2 In the lecture, zip was defined by two equations whose left hand side
--- | patterns overlapped. The order of these two equations matters: what
--- | happens if they are switched? Find a set of defining equations whose
--- | order does not matter.
+-- | 6.2 
 
 zip' :: [a] -> [b] -> [(a,b)]
 zip' (x:xs) (y:ys) = (x,y):zip' xs ys
@@ -147,12 +110,7 @@ zip' _ [] = []
 -- | *Main> zip' [1..3] ['a'..'z']
 -- | [(1,'a'),(2,'b'),(3,'c')]
 
--- | 6.3 The predefined function
--- | zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
--- | clearly, from its type, is related to zip. Give a definition of zipWith in
--- | terms of zip and other standard functions.
--- | In practice, zipWith is defined directly and zip is then defined in terms
--- | of zipWith. Write a recursive definition of zipWith and use it to define zip.
+-- | 6.3
 
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' f as bs = map (uncurry f) $ zip as bs
@@ -177,12 +135,7 @@ zip'' = zipWith'' (\ x y -> (x, y))
 -- | *Main> zip'' [1..3] ['a'..'z']
 -- | [(1,'a'),(2,'b'),(3,'c')]
 
--- | 6.4 Write (perhaps using unfold to do so) a function
--- | > splits :: [a] -> [(a,[a])]
--- | which given a list xs returns a list of all the (x; as ++ bs) that satisfy
--- | as ++ [x] ++ bs = xs so that you can define for non-null xs
--- | > permutations xs =
--- | > [ x:zs | (x,ys) <- splits xs, zs <- permutations ys ]
+-- | 6.4
 
 splits :: [a] -> [(a,[a])]
 splits [] = []
@@ -225,16 +178,7 @@ permutations [] = [[]]
 permutations xs = [z:ys | (z, zs) <- splits xs, ys <- permutations zs]
 
 
--- | 6.5 The function permutation0
--- | > permutations’ :: [a] -> [[a]]
--- | > permutations’ [] = [[]]
--- | > permutations’ (x:xs) =
--- | > [ zs | ys <- permutations’ xs, zs <- include x ys ]
--- | has the form of a fold, as does include
--- | > include :: a -> [a] -> [[a]]
--- | > include x [] = [[x]]
--- | > include x (y:ys) = [x:y:ys] ++ map (y:) (include x ys)
--- | Rewrite them to use fold (or foldr ) and no explicit recursion.
+-- | 6.5 
 
 -- | reminder:
 -- | foldr' :: (a -> b -> b) -> b -> [a] -> b
@@ -251,7 +195,7 @@ permutations' = foldr (\ elem lists -> concat . map (include' elem) $ lists) [[]
 -- | *Main> all (`elem` permutations' [1,2,3,4,5]) (permutations [1,2,3,4,5])
 -- | True
 
--- | First attempt:
+-- | First attempt (almost correct):
 -- | include' x = foldr (\ elem (zs:zss) -> map (elem:) $ (zs:(x:zs):zss)) [[]]
 include' :: a -> [a] -> [[a]]
 include' x = foldr (\ elem ((z:zs):zss) -> (z:elem:zs) : (map (elem:) ((z:zs):zss))) [[x]]
@@ -265,19 +209,7 @@ include' x = foldr (\ elem ((z:zs):zss) -> (z:elem:zs) : (map (elem:) ((z:zs):zs
 -- | *Main> include' 99 [1,2,3]
 -- | [[99,1,2,3],[1,99,2,3],[1,2,99,3],[1,2,3,99]]
 
--- | 6.6 Calls of unfold null head tail are often ineffcient because at least 
--- | two of the parameter functions have to do the same work and each is each is 
--- | obliged to do it for themselves. One possible solution to this property is to 
--- | have a single parameter, say  nht :: a -> Maybe (b, a) which encapsulates the 
--- | work of the three parameters of unfold, together with 
--- | > unfold’ :: (a -> Maybe (b,a)) -> a -> [b] 
--- | for which 
--- | unfold0 nht = unfold n h t 
--- | where the chosen nht depends on the given n and h and t, but crucially need not 
--- | call them separately. Show how to implement selection sort as an instance of 
--- | unfold0 without having to repeat any calls of minimum.
-
-
+-- | 6.6 
 -- reminder:
 -- unfold :: (a->Bool) -> (a->b) -> (a->a) -> a -> [b]
 -- unfold = map head . takeWhile (not.null) . iterate tail
